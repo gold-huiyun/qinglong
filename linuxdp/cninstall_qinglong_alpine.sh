76309ac21b735d14dd21d35f3de013de51427d96
gold-huiyun <90807191+gold-huiyun@users.noreply.github.com>
2025-12-06 20:27:08 +0800

    Update package mirrors in cninstall script

--- linuxdp/cninstall_qinglong_alpine.sh
+++ linuxdp/cninstall_qinglong_alpine.sh
@@ -18,10 +18,11 @@ PYTHON_HOME="${PYTHON_HOME:-${QL_DIR}/data/dep_cache/python3}"
 PYTHON_SHORT_VERSION="${PYTHON_SHORT_VERSION:-3.11}"
 
 # 国内源
-#ALPINE_MIRROR="${ALPINE_MIRROR:-mirrors.tuna.tsinghua.edu.cn}" #清华源
+#ALPINE_MIRROR="${ALPINE_MIRROR:-mirrors.tuna.tsinghua.edu.cn}" #清华源抽风
 ALPINE_MIRROR="${ALPINE_MIRROR:-mirrors.aliyun.com}"
 NPM_REGISTRY="${NPM_REGISTRY:-https://registry.npmmirror.com}"
-PIP_INDEX_URL="${PIP_INDEX_URL:-https://pypi.tuna.tsinghua.edu.cn/simple}"
+#PIP_INDEX_URL="${PIP_INDEX_URL:-https://pypi.tuna.tsinghua.edu.cn/simple}"#清华源抽风
+PIP_INDEX_URL="${PIP_INDEX_URL:-http://mirrors.aliyun.com/pypi/simple}"
 
 # --------------------------
 # 基础优化：Alpine 源、时区
